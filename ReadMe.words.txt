e41bc20eb16ea0889b993f097670b06b7c09650b
Teal <tmowyang@gmail.com>
2015-09-19 13:11:41 -0700

    Added letter textures and placed one letter on the screen

--- ReadMe.words.txt
+++ ReadMe.words.txt
@@ -1,5 +1,5 @@
 Created: 2015-09-19
 By: Teal Owyang
-Using Template: 2D Graphics (lw2DSceneGraph)
+Using Template: 2D Graphics (Iw2DSceneGraph)
 
 A Scrabble Tetris Puzzle Fighter Game
---
+++ source/main.cpp
@@ -0,0 +1,32 @@
+#include "Iw2D.h"
+
+int main()
+{
+	// Initialise the 2D graphics system
+	Iw2DInit();
+
+	// Create an image from a PNG file
+	CIw2DImage* image = Iw2DCreateImage("textures/letter_T.png");
+
+	// Loop forever, until the user or the OS performs some action to quit the app
+	while (!s3eDeviceCheckQuitRequest())
+	{
+		// Clear the drawing surface
+		Iw2DSurfaceClear(0xff000000);
+
+		// Draw an image
+		Iw2DDrawImage(image, CIwFVec2::g_Zero);
+
+		// Show the drawing surface
+		Iw2DSurfaceShow();
+
+		// Yield to the OS
+		s3eDeviceYield(0);
+	}
+
+	// Clean-up
+	delete image;
+	Iw2DTerminate();
+
+	return 0;
+}
